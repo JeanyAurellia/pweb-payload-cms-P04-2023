111ea824d1d36d98d18eb01ab0beb49b962d05ac
weekreee <wikrinew@gmail.com>
2023-10-28 16:51:15 +0700

    Feat: add hooks on category

--- src/collections/Category.js
+++ src/collections/Category.js
@@ -1,3 +1,4 @@
+import payload from 'payload';
 /** @type {import('payload/types').CollectionConfig} */
 
 const Category = {
@@ -5,6 +6,61 @@ const Category = {
   admin: {
     useAsTitle: 'name',
   },
+
+  access: {
+    read: () => true,
+    update: () => true,
+    create: () => true,
+    delete: () => true,
+  },
+
+  hooks: {
+    afterOperation: [
+      async (args) => {
+        if (args.operation == 'create'){
+          payload.create ({
+            collection: 'logs',
+            data: {
+              type: 'Category',
+              Name: args.result.name,
+              Activity: 'Create',
+            },
+          });
+        }
+      },
+    ],
+
+    afterChange: [
+      async (args) => {
+        if (args.operation == 'update'){
+          payload.create ({
+            collection: 'logs',
+            data: {
+              type: 'Category',
+              Name: args.doc.name,
+              Activity: 'Update',
+            },
+          });
+        }
+      },
+    ],
+
+    afterDelete: [
+      async (args) => {
+        payload.create ({
+          collection: 'logs',
+          data: {
+            type: 'Category',
+            Name: args.doc.name,
+            Activity: 'Delete',
+          },
+        });
+      },
+    ],
+  },
+
+  
+
   fields: [
     {
       name: 'name',
